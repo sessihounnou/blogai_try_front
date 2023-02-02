6c4730f8215b913e1db0b3f6c2acc2663aff54d5
dricdrk <cdric.hounnou2@gmail.com>
2023-02-02 17:14:30 -0500

    add search button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,7 +30,7 @@ methods: {
   }
 },
   mounted() {
-    axios.get('http://localhost:8080/articles/2')
+    axios.get('http://localhost:8080/articles/0')
    .then(response => {
       this.apiData = response.data;
       this.apiDataSpliced = this.spliceData();
@@ -44,9 +44,20 @@ methods: {
 </script>
 
 <template>
-  <section>
+<section>
     <!-- Title -->
 <div class="pt-32  bg-white">
+  <div class="relative text-lg bg-transparent text-gray-800 w-32">
+    <div class="flex items-center border-b border-b-2 border-teal-500 py-2">
+    <input class="bg-transparent border-none mr-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Search">
+    <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
+        <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
+            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
+        </svg>
+    </button>
+    </div>
+</div>
+  
 <h1 class="text-center text-2xl font-bold text-gray-800">Toutes Les catégories</h1>
 </div>
 
@@ -88,7 +99,7 @@ methods: {
         <section  class="sm:flex justify-center ">
         <div  class="flex items-center p-4 overflow-hidden " v-for="subitem in item" :key="subitem.id">
           <div class="container mx-auto max-w-sm bg-white rounded-xl shadow-lg  hover:scale-105 hover:shadow-2xl transform transition-all duration-500">
-            <img src="https://images.unsplash.com/photo-1527112862739-c3b9466d902e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=966&q=80" alt="">
+            <img :src="`http://localhost:8080/${subitem.image}`" alt="">
             <div class="p-6">
               <h1 class="text-3xl font-bold text-gray-800 cursor-pointer ">{{subitem.id}}</h1>
               <h2 class="text-xl text-gray-800 font-semibold">{{subitem.title}}</h2>
