026d86be44cb5ab8c36f21f6d335395c020d4b74
Cedric Hounnou <drico@Cedrics-MacBook-Air.local>
2023-03-08 16:32:45 +0100

    make new design or chat feature

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -11,11 +11,12 @@ export default {
   },
   methods: {
     expendTextarea() {
-      this.textareaRow++;
       console.log(this.$refs.prompt.cols);
       // this.$refs.prompt.cols = this.$refs.prompt.value.length - 4;
+
       if (this.$refs.prompt.value.length < 20) {
         this.$refs.prompt.cols++;
+        this.textareaRow++;
       }
       console.log(this.$refs.prompt.value.length);
       let data = {
@@ -36,6 +37,7 @@ export default {
           cleanText = cleanText.replace(/\n/g, "").replace(/\t/g, "    ");
           vm.prompt_response.push(cleanText);
           console.log(vm.prompt_response);
+          vm.$refs.prompt.value = "";
         })
         .catch(function (error) {
           console.log(error);
@@ -45,68 +47,41 @@ export default {
 };
 </script>
 <template>
-  <div class="flex my-8 w-full flex-col items-center justify-center gap-y-2">
+  <div class="flex py-10 w-full flex-col items-center justify-center gap-y-2">
     <div
-      class="w-[60%] rounded-xl border border-gray-200 bg-white py-4 px-2 shadow-md shadow-gray-100"
+      class="w-[50%] rounded-xl border border-gray-200 bg-white py-4 px-2 shadow-md shadow-gray-100"
     >
-      <div v-for="data in prompt_response">
-        <div>{{ data }}</div>
-        <div>------------------</div>
+      <div class="mt-4">
+        <div class="flex max-h-[400px] w-full flex-col overflow-y-scroll">
+          <div v-for="data in prompt_response">
+            <button
+              class="bg-gray-200 my-2 right group flex items-center gap-x-5 rounded-md px-2.5 py-2 transition-all duration-75 hover:bg-gray-100"
+            >
+              <div
+                class="flex flex-col items-start justify-between font-light text-gray-600"
+              >
+                <span class="text-2xl font-medium text-gray-700 font-light">{{
+                  data
+                }}</span>
+              </div>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div
-      class="mx-auto flex w-[60%] items-center justify-start gap-x-3 rounded-lg border border-gray-700 bg-gray-700 px-4 py-2"
-    >
-      <button
-        class="inline-flex items-center space-x-2 rounded-full bg-gray-700 font-semibold text-white"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          aria-hidden="true"
-          role="img"
-          class="w-6"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <g fill="none">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"
-              fill="currentColor"
-            />
-          </g>
-        </svg>
-      </button>
-      <textarea
-        @keydown.enter="expendTextarea"
+    <div class="w-[50%] mb-4 flex items-center">
+      <input
+        type="text"
+        class="w-full rounded-lg border border-gray-400 p-2"
         ref="prompt"
-        cols="78"
-        :rows="textareaRow"
-      ></textarea>
+        placeholder="Search ..."
+        @keypress.e=""
+      />
       <button
-        class="inline-flex items-center space-x-2 rounded-full bg-gray-700 font-semibold text-white"
+        class="ml-2 rounded-lg bg-blue-500 p-2 text-white hover:bg-blue-600"
+        @click="expendTextarea"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          aria-hidden="true"
-          role="img"
-          class="w-6"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <g fill="none">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"
-              fill="currentColor"
-            />
-          </g>
-        </svg>
+        Envoyer
       </button>
     </div>
   </div>
